03c7c8e9e2386113c168a082d5b15f8439755d2e
iitum <watcharaphong@intbizth.com>
2015-07-29 16:08:11 +0700

    edit com.intbizth.balltoro.winloseordraw

--- app/widgets/com.intbizth.balltoro.winloseordraw/controllers/widget.js
+++ app/widgets/com.intbizth.balltoro.winloseordraw/controllers/widget.js
@@ -1,4 +1,7 @@
 var global = {
+	test : {
+		timer : null
+	},
 	width : 15,
 	height : 15,
 	winBackgroundColor : '#32ac41',
@@ -9,48 +12,7 @@ var global = {
 	drawFontColor : '#000'
 };
 
-/**
- *
- * @param {Object} args
- */
-exports.loadConfig = function(args) {
-	if (args.width) {
-		global.width = args.width;
-	}
-
-	if (args.height) {
-		global.height = args.height;
-	}
-
-	if (args.winBackgroundColor) {
-		global.winBackgroundColor = args.winBackgroundColor;
-	}
-
-	if (args.winFontColor) {
-		global.winFontColor = args.winFontColor;
-	}
-
-	if (args.loseBackgroundColor) {
-		global.loseBackgroundColor = args.loseBackgroundColor;
-	}
-
-	if (args.loseFontColor) {
-		global.loseFontColor = args.loseFontColor;
-	}
-
-	if (args.drawBackgroundColor) {
-		global.drawBackgroundColor = args.drawBackgroundColor;
-	}
-
-	if (args.drawFontColor) {
-		global.drawFontColor = args.drawFontColor;
-	}
-};
-
-/**
- * return {UIView}
- */
-exports.createWin = function() {
+function createWin() {
 	var view = Ti.UI.createView({
 		width : global.width,
 		height : global.height,
@@ -73,10 +35,7 @@ exports.createWin = function() {
 	return view;
 };
 
-/**
- * return {UIView}
- */
-exports.createLose = function() {
+function createLose() {
 	var view = Ti.UI.createView({
 		width : global.width,
 		height : global.height,
@@ -99,10 +58,7 @@ exports.createLose = function() {
 	return view;
 };
 
-/**
- * return {UIView}
- */
-exports.createDraw = function() {
+function createDraw() {
 	var view = Ti.UI.createView({
 		width : global.width,
 		height : global.height,
@@ -123,4 +79,80 @@ exports.createDraw = function() {
 	view.add(label);
 
 	return view;
+};
+
+/**
+ *
+ * @param {Object} args
+ */
+exports.loadConfig = function(args) {
+	for (var i in global) {
+		if (args[i]) {
+			global[i] = args[i];
+		}
+	}
+};
+
+/**
+ * @param {Integer} duration
+ */
+exports.startTest = function(duration, element) {
+	run();
+
+	global.timer = setInterval(function() {
+		run();
+	}, duration);
+
+	function run() {
+		element.removeAllChildren();
+
+		var random = _.random(1, 10);
+
+		for (var i = 1; i <= random; i++) {
+			var random2 = _.random(0, 2);
+
+			if (random2 === 0) {
+				element.add(createWin());
+			} else if (random2 === 1) {
+				element.add(createLose());
+			} else if (random2 === 2) {
+				element.add(createDraw());
+			}
+
+			if (i < random) {
+				element.add(Ti.UI.createView({
+					width : 2,
+					height : 1
+				}));
+			}
+		}
+	};
+};
+
+exports.stopTest = function(element) {
+	clearInterval(global.timer);
+	global.timer = null;
+
+	element.removeAllChildren();
+};
+
+/**
+ * return {UIView}
+ */
+exports.createWin = function() {
+	return createWin();
+};
+
+/**
+ * return {UIView}
+ */
+exports.createLose = function() {
+	return createLose();
+};
+
+/**
+ * return {UIView}
+ */
+exports.createDraw = function() {
+	return createDraw();
 };
