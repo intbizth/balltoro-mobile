f8f7add60aa9af0cfb2bba30757a3a249362436e
iitum <watcharaphong@intbizth.com>
2015-07-29 11:47:41 +0700

    edit layout com.intbizth.balltoro.matchsummytable

--- app/widgets/com.intbizth.balltoro.matchsummytable/controllers/widget.js
+++ app/widgets/com.intbizth.balltoro.matchsummytable/controllers/widget.js
@@ -2,6 +2,7 @@ var global = {
 	sectionHeight : 30,
 	rowNoDataHeight : 28,
 	rowHeight : 25,
+	vsTableWidths : [0.14, 0.2, 0.52, 0.14],
 	vsTableSectionBackgroundColor : '#ff3b30',
 	vsTableSectionFontColor : '#fff',
 	vsTableSectionLineColor : '#b22922',
@@ -15,6 +16,7 @@ var global = {
 	vsTableRow2BackgroundColor : '#162a38',
 	vsTableRow2FontColor : '#fff',
 	vsTableRow2FontHighlightColor : '#549af7',
+	matchTableWidths : [0.14, 0.2, 0.52, 0.14],
 	matchTableSectionBackgroundColor : '#549af7',
 	matchTableSectionFontColor : '#fff',
 	matchTableSectionLineColor : '#4781cb',
@@ -28,6 +30,7 @@ var global = {
 	matchTableRow2BackgroundColor : '#162a38',
 	matchTableRow2FontColor : '#fff',
 	matchTableRow2FontHighlightColor : '#549af7',
+	teamTableWidths : [0.08, 0.64, 0.08, 0.08, 0.08, 0.04],
 	teamTableSectionBackgroundColor : '#549af7',
 	teamTableSectionFontColor : '#fff',
 	teamTableSectionLineColor : '#4781cb',
@@ -40,7 +43,21 @@ var global = {
 	teamTableRow1FontHighlightColor : '#549af7',
 	teamTableRow2BackgroundColor : '#162a38',
 	teamTableRow2FontColor : '#fff',
-	teamTableRow2FontHighlightColor : '#549af7'
+	teamTableRow2FontHighlightColor : '#549af7',
+	leagueTableWidths : [0.08, 0.4, 0.08, 0.08, 0.08, 0.08, 0.08, 0.08, 0.04],
+	leagueTableSectionBackgroundColor : '#549af7',
+	leagueTableSectionFontColor : '#fff',
+	leagueTableSectionLineColor : '#4781cb',
+	leagueTableNoDataBackgroundColor : '#383a3b',
+	leagueTableNoDataFontColor : '#fff',
+	leagueTableHeaderBackgroundColor : '#383a3b',
+	leagueTableHeaderFontColor : '#fff',
+	leagueTableRow1BackgroundColor : '#213647',
+	leagueTableRow1FontColor : '#fff',
+	leagueTableRow1FontHighlightColor : '#549af7',
+	leagueTableRow2BackgroundColor : '#162a38',
+	leagueTableRow2FontColor : '#fff',
+	leagueTableRow2FontHighlightColor : '#549af7'
 };
 
 function initialize() {
@@ -105,10 +122,10 @@ function createVsTable(data) {
 			}
 		});
 	} else {
-		var widths = [0.14, 0.2, 0.52, 0.14];
+		var widths = JSON.parse(JSON.stringify(global.vsTableWidths));
 
 		for (var i in widths) {
-			widths[i] = parseInt(Ti.Platform.displayCaps.platformWidth * widths[i]);
+			widths[i] = Ti.Platform.displayCaps.platformWidth * widths[i];
 		}
 
 		items.push({
@@ -204,7 +221,7 @@ function createVsTable(data) {
 					text : data.data[i].team[0]
 				},
 				dashLabel : {
-					color : (i % 2 === 1) ? global.vsTableRow1FontColor : global.vsTableRow2FontColor,
+					color : (i % 2 === 1) ? global.vsTableRow1FontColor : global.vsTableRow2FontColor
 				},
 				team2Label : {
 					width : Ti.UI.SIZE,
@@ -288,10 +305,10 @@ function createMatchTable(data) {
 			}
 		});
 	} else {
-		var widths = [0.14, 0.2, 0.52, 0.14];
+		var widths = JSON.parse(JSON.stringify(global.matchTableWidths));
 
 		for (var i in widths) {
-			widths[i] = parseInt(Ti.Platform.displayCaps.platformWidth * widths[i]);
+			widths[i] = Ti.Platform.displayCaps.platformWidth * widths[i];
 		}
 
 		items.push({
@@ -387,7 +404,7 @@ function createMatchTable(data) {
 					text : data.data[i].team[0]
 				},
 				dashLabel : {
-					color : (i % 2 === 1) ? global.matchTableRow1FontColor : global.matchTableRow2FontColor,
+					color : (i % 2 === 1) ? global.matchTableRow1FontColor : global.matchTableRow2FontColor
 				},
 				team2Label : {
 					width : Ti.UI.SIZE,
@@ -471,10 +488,10 @@ function createTeamTable(data) {
 			}
 		});
 	} else {
-		var widths = [0.14, 0.46, 0.1, 0.1, 0.1, 0.1];
+		var widths = JSON.parse(JSON.stringify(global.teamTableWidths));
 
 		for (var i in widths) {
-			widths[i] = parseInt(Ti.Platform.displayCaps.platformWidth * widths[i]);
+			widths[i] = Ti.Platform.displayCaps.platformWidth * widths[i];
 		}
 
 		items.push({
@@ -514,12 +531,12 @@ function createTeamTable(data) {
 				height : global.rowHeight,
 				color : global.teamTableHeaderFontColor
 			},
-			goalGifferenceView : {
+			goalDifferenceView : {
 				left : widths[0] + widths[1] + widths[2],
 				width : widths[3],
 				height : global.rowHeight
 			},
-			goalGifferenceLabel : {
+			goalDifferenceLabel : {
 				width : widths[3] - 2,
 				height : global.rowHeight,
 				color : global.teamTableHeaderFontColor
@@ -589,12 +606,12 @@ function createTeamTable(data) {
 					color : (data.data[i].highlight === 0) ? global.teamTableRow1FontHighlightColor : ((i % 2 === 1) ? global.teamTableRow1FontColor : global.teamTableRow2FontColor),
 					text : data.data[i].played
 				},
-				goalGifferenceView : {
+				goalDifferenceView : {
 					left : widths[0] + widths[1] + widths[2],
 					width : widths[3],
 					height : global.rowHeight
 				},
-				goalGifferenceLabel : {
+				goalDifferenceLabel : {
 					width : Ti.UI.SIZE,
 					height : global.rowHeight,
 					color : (data.data[i].highlight === 0) ? global.teamTableRow1FontHighlightColor : ((i % 2 === 1) ? global.teamTableRow1FontColor : global.teamTableRow2FontColor),
@@ -617,7 +634,7 @@ function createTeamTable(data) {
 					height : global.rowHeight
 				},
 				rankingImage : {
-					image : (data.data[i].ranking === -1) ? 'images/com.intbizth.balltoro.matchsummytable/btn_down.png' : ((data.data[i].ranking === 1) ? 'images/com.intbizth.balltoro.matchsummytable/btn_up.png' : '')
+					image : (data.data[i].ranking === -1) ? WPATH('images/btn_down.png') : ((data.data[i].ranking === 1) ? WPATH('images/btn_up.png') : '')
 				}
 			});
 		}
@@ -629,9 +646,277 @@ function createTeamTable(data) {
 };
 
 function createLeagueTable(data) {
-	var section = Ti.UI.createListSection({
-
+	var headerView = Ti.UI.createView({
+		width : Ti.UI.FILL,
+		height : global.sectionHeight,
+		backgroundColor : global.leagueTableSectionBackgroundColor
 	});
+
+	var headerLabel = Ti.UI.createLabel({
+		top : 0,
+		left : 4,
+		width : Ti.Platform.displayCaps.platformWidth - 8,
+		height : headerView.height,
+		color : global.leagueTableSectionFontColor,
+		font : {
+			fontSize : 14
+		},
+		text : (data.headerTitle) ? data.headerTitle : '',
+		textAlign : 'left'
+	});
+
+	headerView.add(headerLabel);
+
+	var headerLine = Ti.UI.createView({
+		bottom : 0,
+		width : Ti.UI.FILL,
+		height : 1,
+		backgroundColor : global.leagueTableSectionLineColor
+	});
+
+	headerView.add(headerLine);
+
+	var section = Ti.UI.createListSection({
+		headerView : headerView
+	});
+
+	var items = [];
+
+	if (data.data.length === 0) {
+		items.push({
+			template : 'leagueTableNoData',
+			properties : {
+				height : global.rowNoDataHeight,
+				backgroundColor : global.leagueTableNoDataBackgroundColor,
+				selectionStyle : Ti.UI.iPhone.ListViewCellSelectionStyle.NONE
+			},
+			noDataView : {
+				width : Ti.UI.FILL,
+				height : global.rowNoDataHeight
+			},
+			noDataLabel : {
+				width : Ti.Platform.displayCaps.platformWidth - 8,
+				height : global.rowNoDataHeight,
+				color : global.leagueTableNoDataFontColor
+			}
+		});
+	} else {
+		var widths = JSON.parse(JSON.stringify(global.leagueTableWidths));
+
+		for (var i in widths) {
+			widths[i] = Ti.Platform.displayCaps.platformWidth * widths[i];
+		}
+
+		items.push({
+			template : 'leagueTableHeader',
+			properties : {
+				height : global.rowHeight,
+				backgroundColor : global.leagueTableHeaderBackgroundColor,
+				selectionStyle : Ti.UI.iPhone.ListViewCellSelectionStyle.NONE
+			},
+			positionView : {
+				left : 0,
+				width : widths[0],
+				height : global.rowHeight
+			},
+			positionLabel : {
+				width : widths[0] - 2,
+				height : global.rowHeight,
+				color : global.leagueTableHeaderFontColor
+			},
+			teamView : {
+				left : widths[0],
+				width : widths[1],
+				height : global.rowHeight
+			},
+			teamLabel : {
+				width : widths[1] - 2,
+				height : global.rowHeight,
+				color : global.leagueTableHeaderFontColor
+			},
+			playedView : {
+				left : widths[0] + widths[1],
+				width : widths[2],
+				height : global.rowHeight
+			},
+			playedLabel : {
+				width : widths[2] - 2,
+				height : global.rowHeight,
+				color : global.leagueTableHeaderFontColor
+			},
+			winView : {
+				left : widths[0] + widths[1] + widths[2],
+				width : widths[3],
+				height : global.rowHeight
+			},
+			winLabel : {
+				width : widths[3] - 2,
+				height : global.rowHeight,
+				color : global.leagueTableHeaderFontColor
+			},
+			drawView : {
+				left : widths[0] + widths[1] + widths[2] + widths[3],
+				width : widths[4],
+				height : global.rowHeight
+			},
+			drawLabel : {
+				width : widths[4] - 2,
+				height : global.rowHeight,
+				color : global.leagueTableHeaderFontColor
+			},
+			loseView : {
+				left : widths[0] + widths[1] + widths[2] + widths[3] + widths[4],
+				width : widths[5],
+				height : global.rowHeight
+			},
+			loseLabel : {
+				width : widths[5] - 2,
+				height : global.rowHeight,
+				color : global.leagueTableHeaderFontColor
+			},
+			goalDifferenceView : {
+				left : widths[0] + widths[1] + widths[2] + widths[3] + widths[4] + widths[5],
+				width : widths[6],
+				height : global.rowHeight
+			},
+			goalDifferenceLabel : {
+				width : widths[6] - 2,
+				height : global.rowHeight,
+				color : global.leagueTableHeaderFontColor
+			},
+			pointsView : {
+				left : widths[0] + widths[1] + widths[2] + widths[3] + widths[4] + widths[5] + widths[6],
+				width : widths[7],
+				height : global.rowHeight
+			},
+			pointsLabel : {
+				width : widths[7] - 2,
+				height : global.rowHeight,
+				color : global.leagueTableHeaderFontColor
+			},
+			rankingView : {
+				left : widths[0] + widths[1] + widths[2] + widths[3] + widths[4] + widths[5] + widths[6] + widths[7],
+				width : widths[8],
+				height : global.rowHeight
+			},
+			rankingLabel : {
+				width : widths[8] - 2,
+				height : global.rowHeight,
+				color : global.leagueTableHeaderFontColor
+			}
+		});
+
+		for (var i in data.data) {
+			i = parseInt(i);
+
+			items.push({
+				template : 'leagueTableRow',
+				properties : {
+					height : global.rowHeight,
+					backgroundColor : (i % 2 === 1) ? global.leagueTableRow1BackgroundColor : global.leagueTableRow2BackgroundColor,
+					selectionStyle : Ti.UI.iPhone.ListViewCellSelectionStyle.NONE
+				},
+				positionView : {
+					left : 0,
+					width : widths[0],
+					height : global.rowHeight
+				},
+				positionLabel : {
+					width : widths[0] - 2,
+					height : global.rowHeight,
+					color : (i % 2 === 1) ? global.leagueTableRow1FontColor : global.leagueTableRow2FontColor,
+					text : data.data[i].position
+				},
+				teamView : {
+					left : widths[0],
+					width : widths[1],
+					height : global.rowHeight
+				},
+				teamLabel : {
+					width : widths[1] - 2,
+					height : global.rowHeight,
+					color : (i % 2 === 1) ? global.leagueTableRow1FontColor : global.leagueTableRow2FontColor,
+					text : data.data[i].team
+				},
+				playedView : {
+					left : widths[0] + widths[1],
+					width : widths[2],
+					height : global.rowHeight
+				},
+				playedLabel : {
+					width : widths[2] - 2,
+					height : global.rowHeight,
+					color : (i % 2 === 1) ? global.leagueTableRow1FontColor : global.leagueTableRow2FontColor,
+					text : data.data[i].played
+				},
+				winView : {
+					left : widths[0] + widths[1] + widths[2],
+					width : widths[3],
+					height : global.rowHeight
+				},
+				winLabel : {
+					width : widths[3] - 2,
+					height : global.rowHeight,
+					color : (i % 2 === 1) ? global.leagueTableRow1FontColor : global.leagueTableRow2FontColor,
+					text : data.data[i].win
+				},
+				drawView : {
+					left : widths[0] + widths[1] + widths[2] + widths[3],
+					width : widths[4],
+					height : global.rowHeight
+				},
+				drawLabel : {
+					width : widths[4] - 2,
+					height : global.rowHeight,
+					color : (i % 2 === 1) ? global.leagueTableRow1FontColor : global.leagueTableRow2FontColor,
+					text : data.data[i].draw
+				},
+				loseView : {
+					left : widths[0] + widths[1] + widths[2] + widths[3] + widths[4],
+					width : widths[5],
+					height : global.rowHeight
+				},
+				loseLabel : {
+					width : widths[5] - 2,
+					height : global.rowHeight,
+					color : (i % 2 === 1) ? global.leagueTableRow1FontColor : global.leagueTableRow2FontColor,
+					text : data.data[i].lose
+				},
+				goalDifferenceView : {
+					left : widths[0] + widths[1] + widths[2] + widths[3] + widths[4] + widths[5],
+					width : widths[6],
+					height : global.rowHeight
+				},
+				goalDifferenceLabel : {
+					width : widths[6] - 2,
+					height : global.rowHeight,
+					color : (i % 2 === 1) ? global.leagueTableRow1FontColor : global.leagueTableRow2FontColor,
+					text : data.data[i].goal_difference
+				},
+				pointsView : {
+					left : widths[0] + widths[1] + widths[2] + widths[3] + widths[4] + widths[5] + widths[6],
+					width : widths[7],
+					height : global.rowHeight
+				},
+				pointsLabel : {
+					width : widths[7] - 2,
+					height : global.rowHeight,
+					color : (i % 2 === 1) ? global.leagueTableRow1FontColor : global.leagueTableRow2FontColor,
+					text : data.data[i].points
+				},
+				rankingView : {
+					left : widths[0] + widths[1] + widths[2] + widths[3] + widths[4] + widths[5] + widths[6] + widths[7],
+					width : widths[8],
+					height : global.rowHeight
+				},
+				rankingImage : {
+					image : (data.data[i].ranking === -1) ? WPATH('images/btn_down.png') : ((data.data[i].ranking === 1) ? WPATH('images/btn_up.png') : '')
+				}
+			});
+		}
+	}
+
+	section.items = items;
 
 	return section;
 };
@@ -665,7 +950,7 @@ exports.setData = function(datas) {
 		} else if (datas[i].table === 'team') {
 			sections.push(createTeamTable(datas[i]));
 		} else if (datas[i].table === 'league') {
-			// sections.push(createLeagueTable(datas[i]));
+			sections.push(createLeagueTable(datas[i]));
 		}
 	}
 
