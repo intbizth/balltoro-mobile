79978dd37ff68f7d22e99d0d2d7aa52ab2e19543
iitum <watcharaphong@intbizth.com>
2015-08-04 17:51:22 +0700

    create widget com.intbizth.alloy.webservice

---
+++ app/widgets/com.intbizth.alloy.webservice/lib/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+	debug : true,
+	console : {
+		load : false,
+		error : false,
+		sendstream : false,
+		datastream : false,
+	},
+	timeout : 60000,
+	headers : {
+		APP : 'Balltoro'
+	}
+};
