b7559e3855d805a2d870a6fe0b5dc135ecda23ab
iitum <watcharaphong@intbizth.com>
2015-08-19 14:54:53 +0700

    [com.intbizth.balltoro.matchlabel] change format

--- app/widgets/com.intbizth.balltoro.matchlabel/controllers/widget.js
+++ app/widgets/com.intbizth.balltoro.matchlabel/controllers/widget.js
@@ -15,286 +15,291 @@ var fetchNextPage = function() {
 };
 
 $.list.addEventListener('marker', function(e) {
-	if (debug) {
-		Ti.API.debug('[' + Widget.widgetId + ']', 'marker:start', 'listPulling:', listPulling, 'listMarking:', listMarking);
-	}
+    if (debug) {
+        Ti.API.debug('[' + Widget.widgetId + ']', 'marker:start', 'listPulling:', listPulling, 'listMarking:', listMarking);
+    }
 
-	if (listPulling || listMarking) {
-		return;
-	}
+    if (listPulling || listMarking) {
+        return;
+    }
 
-	listMarking = true;
+    listMarking = true;
 
-	if (debug) {
-		Ti.API.debug('[' + Widget.widgetId + ']', 'marker', e);
-	}
+    if (debug) {
+        Ti.API.debug('[' + Widget.widgetId + ']', 'marker', e);
+    }
 
-	fetchNextPage(function() {
-		listMarking = false;
+    fetchNextPage(function() {
+        listMarking = false;
 
-		if (debug) {
-			Ti.API.debug('[' + Widget.widgetId + ']', 'marker:end');
-		}
-	});
+        if (debug) {
+            Ti.API.debug('[' + Widget.widgetId + ']', 'marker:end');
+        }
+    });
 });
 
 function extendData(models) {
-	if (models.length === 0) {
-		models = [models];
-	}
+    if (models.length === 0) {
+        models = [models];
+    }
 
-	for (var i in models) {
-		var data = {};
-		var dataModel = models[i].toJSON();
+    for (var i in models) {
+        var data = {};
+        var dataModel = models[i].toJSON();
 
-		data = _.extend(data, getBackground(dataModel.template));
+        data = _.extend(data, getBackground(dataModel.template));
 
-		models[i].set(data);
-		models[i].save();
-	}
+        console.error('dataModel:', dataModel);
+
+        models[i].set(data);
+        models[i].save();
+    }
 };
 
 function getBackground(template) {
-	var classes = ['_leftView', '_centerView', '_rightView'];
-	var data = {};
+    var classes = ['_leftView', '_centerView', '_rightView'];
+    var data = {};
 
-	for (var i in classes) {
-		var backgroundProps = ['backgroundColor', 'backgroundColorInAct', 'backgroundColorAct'];
-		var style = $.createStyle({
-			classes : template + classes[i]
-		});
+    for (var i in classes) {
+        var backgroundProps = ['backgroundColor', 'backgroundColorInAct', 'backgroundColorAct'];
+        var style = $.createStyle({
+            classes : template + classes[i]
+        });
 
-		for (var j in backgroundProps) {
-			var prop = classes[i].replace('_', '') + Widget.string.ucfirst(backgroundProps[j]);
+        for (var j in backgroundProps) {
+            var prop = classes[i].replace('_', '') + Widget.string.ucfirst(backgroundProps[j]);
 
-			data[prop] = style[backgroundProps[j]];
-		}
-	}
+            data[prop] = style[backgroundProps[j]];
+        }
+    }
 
-	return data;
+    return data;
 };
 
 function pull(e) {
-	if (debug) {
-		Ti.API.debug('[' + Widget.widgetId + ']', 'pull:start');
-	}
+    if (debug) {
+        Ti.API.debug('[' + Widget.widgetId + ']', 'pull:start');
+    }
 
-	if (e.active) {
-		$.activityIndicator.animate({
-			transform : $.UI.create('2DMatrix').scale(1.5),
-			duration : 600
-		});
-	} else {
-		$.activityIndicator.animate({
-			transform : $.UI.create('2DMatrix').scale(1),
-			duration : 200
-		});
+    if (e.active) {
+        $.activityIndicator.animate({
+            transform : $.UI.create('2DMatrix').scale(1.5),
+            duration : 600
+        });
+    } else {
+        $.activityIndicator.animate({
+            transform : $.UI.create('2DMatrix').scale(1),
+            duration : 200
+        });
 
-		listPulling = false;
-	}
+        listPulling = false;
+    }
 };
 
 function pullend(e) {
-	if (debug) {
-		Ti.API.debug('[' + Widget.widgetId + ']', 'pullend:start', 'listPulling:', listPulling, 'listMarking:', listMarking);
-	}
+    if (debug) {
+        Ti.API.debug('[' + Widget.widgetId + ']', 'pullend:start', 'listPulling:', listPulling, 'listMarking:', listMarking);
+    }
 
-	if (listPulling || listMarking) {
-		return;
-	}
+    if (listPulling || listMarking) {
+        return;
+    }
 
-	listPulling = true;
+    listPulling = true;
 
-	if (debug) {
-		Ti.API.debug('[' + Widget.widgetId + ']', 'pullend', e);
-	}
+    if (debug) {
+        Ti.API.debug('[' + Widget.widgetId + ']', 'pullend', e);
+    }
 
-	$.list.setContentInsets({
-		top : 50
-	}, {
-		animated : true
-	});
+    $.list.setContentInsets({
+        top : 50
+    }, {
+        animated : true
+    });
 
-	clearInterval(listTimer);
-	listTimer = null;
-	listCount = 0;
+    clearInterval(listTimer);
+    listTimer = null;
+    listCount = 0;
 
-	listTimer = setInterval(function() {
-		listCount++;
+    listTimer = setInterval(function() {
+        listCount++;
 
-		if (listCount >= 2) {
-			fetchFirstPage(function() {
-				$.list.setContentInsets({
-					top : 0
-				}, {
-					animated : true
-				});
+        if (listCount >= 2) {
+            fetchFirstPage(function() {
+                $.list.setContentInsets({
+                    top : 0
+                }, {
+                    animated : true
+                });
 
-				$.activityIndicator.transform = $.UI.create('2DMatrix').scale(1);
+                $.activityIndicator.transform = $.UI.create('2DMatrix').scale(1);
 
-				clearInterval(listTimer);
-				listPulling = false;
-				listTimer = null;
-				listCount = 0;
+                clearInterval(listTimer);
+                listPulling = false;
+                listTimer = null;
+                listCount = 0;
 
-				if (debug) {
-					Ti.API.debug('[' + Widget.widgetId + ']', 'pullend:end');
-				}
-			});
-		}
-	}, 1000);
+                if (debug) {
+                    Ti.API.debug('[' + Widget.widgetId + ']', 'pullend:end');
+                }
+            });
+        }
+    }, 1000);
 };
 
 function itemclick(e) {
-	clickCount++;
+    clickCount++;
 
-	var item = $.section.getItemAt(e.itemIndex);
+    var item = $.section.getItemAt(e.itemIndex);
 
-	if (debug) {
-		Ti.API.debug('[' + Widget.widgetId + ']', 'itemclick:e:', e);
-		Ti.API.debug('[' + Widget.widgetId + ']', 'itemclick:item:', item);
-		Ti.API.debug('[' + Widget.widgetId + ']', 'itemclick:clickCount:', clickCount);
-	}
+    if (debug) {
+        Ti.API.debug('[' + Widget.widgetId + ']', 'itemclick:e:', e);
+        Ti.API.debug('[' + Widget.widgetId + ']', 'itemclick:item:', item);
+        Ti.API.debug('[' + Widget.widgetId + ']', 'itemclick:clickCount:', clickCount);
+    }
 
-	function clear() {
-		clickCount = 0;
-		clearTimeout(clickTimer);
-		clickTimer = null;
-	};
+    function clear() {
+        clickCount = 0;
+        clearTimeout(clickTimer);
+        clickTimer = null;
+    };
 
-	if (clickCount === 1) {
-		clickTimer = _.delay(function() {
-			item.leftView.backgroundColor = item.leftView.backgroundColorAct;
-			item.centerView.backgroundColor = item.centerView.backgroundColorAct;
-			item.rightView.backgroundColor = item.rightView.backgroundColorAct;
-			$.section.updateItemAt(e.itemIndex, item);
+    if (clickCount === 1) {
+        clickTimer = _.delay(function() {
+            item.leftView.backgroundColor = item.leftView.backgroundColorAct;
+            item.centerView.backgroundColor = item.centerView.backgroundColorAct;
+            item.rightView.backgroundColor = item.rightView.backgroundColorAct;
+            $.section.updateItemAt(e.itemIndex, item);
 
-			_.delay(function() {
-				item.leftView.backgroundColor = item.leftView.backgroundColorInAct;
-				item.centerView.backgroundColor = item.centerView.backgroundColorInAct;
-				item.rightView.backgroundColor = item.rightView.backgroundColorInAct;
-				$.section.updateItemAt(e.itemIndex, item);
-			}, 400);
+            _.delay(function() {
+                item.leftView.backgroundColor = item.leftView.backgroundColorInAct;
+                item.centerView.backgroundColor = item.centerView.backgroundColorInAct;
+                item.rightView.backgroundColor = item.rightView.backgroundColorInAct;
+                $.section.updateItemAt(e.itemIndex, item);
+            }, 400);
 
-			clear();
-		}, 400);
-	} else {
-		item.leftView.backgroundColor = item.leftView.backgroundColorAct;
-		item.centerView.backgroundColor = item.centerView.backgroundColorAct;
-		item.rightView.backgroundColor = item.rightView.backgroundColorAct;
-		$.section.updateItemAt(e.itemIndex, item);
+            $.trigger('dblclick', e);
 
-		_.delay(function() {
-			item.leftView.backgroundColor = item.leftView.backgroundColorInAct;
-			item.centerView.backgroundColor = item.centerView.backgroundColorInAct;
-			item.rightView.backgroundColor = item.rightView.backgroundColorInAct;
-			$.section.updateItemAt(e.itemIndex, item);
-		}, 400);
+            clear();
+        }, 400);
+    } else {
+        item.leftView.backgroundColor = item.leftView.backgroundColorAct;
+        item.centerView.backgroundColor = item.centerView.backgroundColorAct;
+        item.rightView.backgroundColor = item.rightView.backgroundColorAct;
+        $.section.updateItemAt(e.itemIndex, item);
 
-		clear();
-	}
+        _.delay(function() {
+            item.leftView.backgroundColor = item.leftView.backgroundColorInAct;
+            item.centerView.backgroundColor = item.centerView.backgroundColorInAct;
+            item.rightView.backgroundColor = item.rightView.backgroundColorInAct;
+            $.section.updateItemAt(e.itemIndex, item);
+        }, 400);
+
+        $.trigger('click', e);
+
+        clear();
+    }
 };
 
 function add(args) {
-	if (debug) {
-		Ti.API.debug('[' + Widget.widgetId + ']', 'add', args);
-	}
+    if (debug) {
+        Ti.API.debug('[' + Widget.widgetId + ']', 'add', args);
+    }
 
-	if (args.data.length > 0) {
-		var marker = {
-			sectionIndex : 0,
-			itemIndex : args.data.length - 1
-		};
+    if (args.data.length > 0) {
+        var marker = {
+            sectionIndex : 0,
+            itemIndex : ($.section.items.length + args.data.length) - 1
+        };
 
-		if (debug) {
-			Ti.API.debug('[' + Widget.widgetId + ']', 'addMarker:', marker);
-		}
+        if (debug) {
+            Ti.API.debug('[' + Widget.widgetId + ']', 'addMarker:', marker);
+        }
 
-		$.list.addMarker({
-			sectionIndex : 0,
-			itemIndex : ($.section.items.length + args.data.length) - 1
-		});
-	}
+        $.list.addMarker(marker);
+    }
 
-	fetchNextPage = args.fetchNextPage;
+    fetchNextPage = args.fetchNextPage;
 
-	Widget.Collections.matchelabel.add(args.data);
+    Widget.Collections.matchelabel.add(args.data);
 
-	if (debug) {
-		Ti.API.debug('[' + Widget.widgetId + ']', 'load:after:matchelabel:', Widget.Collections.matchelabel.toJSON());
-	}
+    extendData(Widget.Collections.matchelabel.models);
+
+    if (debug) {
+        Ti.API.debug('[' + Widget.widgetId + ']', 'load:after:matchelabel:', Widget.Collections.matchelabel.toJSON());
+    }
 };
 
 function load(args) {
-	loaded = true;
+    loaded = true;
 
-	if (debug) {
-		Ti.API.debug('[' + Widget.widgetId + ']', 'load', args);
-	}
+    if (debug) {
+        Ti.API.debug('[' + Widget.widgetId + ']', 'load', args);
+    }
 
-	fetchFirstPage = args.fetchFirstPage;
-	fetchNextPage = args.fetchNextPage;
+    fetchFirstPage = args.fetchFirstPage;
+    fetchNextPage = args.fetchNextPage;
 
-	if (!args.noData) {
-		var marker = {
-			sectionIndex : 0,
-			itemIndex : args.data.length - 1
-		};
+    if (!args.noData) {
+        var marker = {
+            sectionIndex : 0,
+            itemIndex : args.data.length - 1
+        };
 
-		if (debug) {
-			Ti.API.debug('[' + Widget.widgetId + ']', 'addMarker:', marker);
-		}
+        if (debug) {
+            Ti.API.debug('[' + Widget.widgetId + ']', 'addMarker:', marker);
+        }
 
-		$.list.addMarker(marker);
-	}
+        $.list.addMarker(marker);
+    }
 
-	Widget.Collections.matchelabel.reset(args.data);
+    Widget.Collections.matchelabel.reset(args.data);
 
-	if (debug) {
-		Ti.API.debug('[' + Widget.widgetId + ']', 'load:before:matchelabel:', Widget.Collections.matchelabel.toJSON());
-	}
+    if (debug) {
+        Ti.API.debug('[' + Widget.widgetId + ']', 'load:before:matchelabel:', Widget.Collections.matchelabel.toJSON());
+    }
 
-	extendData(Widget.Collections.matchelabel.models);
+    extendData(Widget.Collections.matchelabel.models);
 
-	if (debug) {
-		Ti.API.debug('[' + Widget.widgetId + ']', 'load:after:matchelabel:', Widget.Collections.matchelabel.toJSON());
-	}
+    if (debug) {
+        Ti.API.debug('[' + Widget.widgetId + ']', 'load:after:matchelabel:', Widget.Collections.matchelabel.toJSON());
+    }
 };
 
 function unLoad() {
-	loaded = false;
+    loaded = false;
 
-	if (debug) {
-		Ti.API.debug('[' + Widget.widgetId + ']', 'unLoad');
-	}
+    if (debug) {
+        Ti.API.debug('[' + Widget.widgetId + ']', 'unLoad');
+    }
 
-	Widget.Collections.matchelabel.reset([]);
+    Widget.Collections.matchelabel.reset([]);
 
-	if (debug) {
-		Ti.API.debug('[' + Widget.widgetId + ']', 'unLoad:matchelabel:', Widget.Collections.matchelabel.toJSON());
-	}
+    if (debug) {
+        Ti.API.debug('[' + Widget.widgetId + ']', 'unLoad:matchelabel:', Widget.Collections.matchelabel.toJSON());
+    }
 };
 
 exports.getLoad = function() {
-	return loaded;
+    return loaded;
 };
 
 exports.add = function(args) {
-	add(args);
+    add(args);
 };
 
 exports.load = function(args) {
-	load(args);
+    load(args);
 };
 
 exports.unLoad = function() {
-	unLoad();
+    unLoad();
 };
 
 exports.scrollToTop = function() {
-	$.list.scrollToItem(0, 0);
+    $.list.scrollToItem(0, 0);
 };
 
 exports.scrollToBottom = function() {
-	$.list.scrollToItem(0, $.section.items.length - 1);
+    $.list.scrollToItem(0, $.section.items.length - 1);
 };
