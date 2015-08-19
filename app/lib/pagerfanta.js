31175ac0a231da5f9bf68d5ee0a65807d4b9f8a1
iitum <watcharaphong@intbizth.com>
2015-08-19 14:33:59 +0700

    change format the lib

--- app/lib/pagerfanta.js
+++ app/lib/pagerfanta.js
@@ -1,51 +1,51 @@
 exports.setPagerfanta = function(response) {
-	var pagerfanta = {
-		page : null,
-		self : null,
-		first : null,
-		last : null,
-		next : null,
-		previous : null
-	};
-	
-	Ti.API.error('response:', response);
+    var pagerfanta = {
+        page : null,
+        self : null,
+        first : null,
+        last : null,
+        next : null,
+        previous : null
+    };
 
-	try {
-		response = JSON.parse(response);
-		
-		Ti.API.error('response:', response);
+    Ti.API.error('response:', response);
 
-		if (response && response._links) {
-			if (response.page) {
-				pagerfanta.page = response.page;
-			}
+    try {
+        response = JSON.parse(response);
 
-			if (response._links.self && response._links.self.href) {
-				pagerfanta.self = response._links.self.href;
-			}
+        Ti.API.error('response:', response);
 
-			if (response._links.first && response._links.first.href) {
-				pagerfanta.first = response._links.first.href;
-			}
+        if (response && response._links) {
+            if (response.page) {
+                pagerfanta.page = response.page;
+            }
 
-			if (response._links.last && response._links.last.href) {
-				pagerfanta.last = response._links.last.href;
-			}
+            if (response._links.self && response._links.self.href) {
+                pagerfanta.self = response._links.self.href;
+            }
 
-			if (response._links.next && response._links.next.href) {
-				pagerfanta.next = response._links.next.href;
-			}
+            if (response._links.first && response._links.first.href) {
+                pagerfanta.first = response._links.first.href;
+            }
 
-			if (response._links.previous && response._links.previous.href) {
-				pagerfanta.previous = response._links.previous.href;
-			}
-		}
-	} catch (e) {
-	}
+            if (response._links.last && response._links.last.href) {
+                pagerfanta.last = response._links.last.href;
+            }
 
-	Ti.API.error('pagerfanta:', pagerfanta);
+            if (response._links.next && response._links.next.href) {
+                pagerfanta.next = response._links.next.href;
+            }
 
-	return {
-		pagerfanta : pagerfanta
-	};
+            if (response._links.previous && response._links.previous.href) {
+                pagerfanta.previous = response._links.previous.href;
+            }
+        }
+    } catch (e) {
+    }
+
+    Ti.API.error('pagerfanta:', pagerfanta);
+
+    return {
+        pagerfanta : pagerfanta
+    };
 };
--- app/lib/webservice.js
+++ app/lib/webservice.js
@@ -1,267 +1,267 @@
 var config = {
-	debug : true,
-	console : {
-		load : false,
-		error : false,
-		sendstream : false,
-		datastream : false,
-	},
-	timeout : 60000,
-	headers : {
-		APP : 'Balltoro'
-	}
+    debug : true,
+    console : {
+        load : false,
+        error : false,
+        sendstream : false,
+        datastream : false,
+    },
+    timeout : 60000,
+    headers : {
+        APP : 'Balltoro'
+    }
 };
 var session = creataSession();
 
 if (config.debug) {
-	Ti.API.debug('');
-	Ti.API.debug(session, '->', 'Web Service startup');
-	Ti.API.debug('');
+    Ti.API.debug('');
+    Ti.API.debug(session, '->', 'Web Service startup');
+    Ti.API.debug('');
 }
 
 var xhr = Ti.Network.createHTTPClient({
-	timeout : config.timeout
+    timeout : config.timeout
 });
 
 function creataSession() {
-	return new Date().getTime();
+    return new Date().getTime();
 };
 
 function isJSON(value) {
-	try {
-		var json = JSON.parse(value);
-	} catch (e) {
-		return false;
-	}
+    try {
+        var json = JSON.parse(value);
+    } catch (e) {
+        return false;
+    }
 
-	return true;
+    return true;
 };
 
 function callback(status, fn) {
-	switch (status) {
-	case 'success':
-		xhr.onload = function(e) {
-			if (config.debug) {
-				Ti.API.debug('');
-				Ti.API.debug(session, '->', 'Web Service [response <--]');
-				Ti.API.debug(session, '->', 'Success');
-				Ti.API.debug(session, '->', 'Response Text', ':', this.responseText, typeof this.responseText + '[' + ((_.isString(this.responseText)) ? this.responseText.length : 0) + ']');
-			}
+    switch (status) {
+    case 'success':
+        xhr.onload = function(e) {
+            if (config.debug) {
+                Ti.API.debug('');
+                Ti.API.debug(session, '->', 'Web Service [response <--]');
+                Ti.API.debug(session, '->', 'Success');
+                Ti.API.debug(session, '->', 'Response Text', ':', this.responseText, typeof this.responseText + '[' + ((_.isString(this.responseText)) ? this.responseText.length : 0) + ']');
+            }
 
-			var data = null;
+            var data = null;
 
-			if (isJSON(this.responseText)) {
-				data = JSON.parse(this.responseText);
-			}
+            if (isJSON(this.responseText)) {
+                data = JSON.parse(this.responseText);
+            }
 
-			fn(data);
+            fn(data);
 
-			if (config.debug) {
-				Ti.API.debug(session, '->', 'Data', ':', data, typeof data + '[' + ((_.isString(data)) ? data.length : 0) + ']');
-				Ti.API.debug('');
-			}
-		};
+            if (config.debug) {
+                Ti.API.debug(session, '->', 'Data', ':', data, typeof data + '[' + ((_.isString(data)) ? data.length : 0) + ']');
+                Ti.API.debug('');
+            }
+        };
 
-		break;
-	case 'error':
-		xhr.onerror = function(e) {
-			if (config.debug) {
-				Ti.API.error('');
-				Ti.API.error(session, '->', 'Web Service [response <--]');
-				Ti.API.error(session, '->', 'Network Error');
+        break;
+    case 'error':
+        xhr.onerror = function(e) {
+            if (config.debug) {
+                Ti.API.error('');
+                Ti.API.error(session, '->', 'Web Service [response <--]');
+                Ti.API.error(session, '->', 'Network Error');
 
-				if (OS_ANDROID) {
-					Ti.API.error(session, '->', 'Error', typeof e, JSON.stringify(e));
-				} else if (OS_IOS) {
-					Ti.API.error(session, '->', 'Error', typeof e, e);
-				}
+                if (OS_ANDROID) {
+                    Ti.API.error(session, '->', 'Error', typeof e, JSON.stringify(e));
+                } else if (OS_IOS) {
+                    Ti.API.error(session, '->', 'Error', typeof e, e);
+                }
 
-				Ti.API.error('');
-			}
+                Ti.API.error('');
+            }
 
-			if (OS_ANDROID) {
-				if (!_.isUndefined(e.source) && !_.isUndefined(e.source.status)) {
-					e.code = e.source.status;
-				}
-			}
+            if (OS_ANDROID) {
+                if (!_.isUndefined(e.source) && !_.isUndefined(e.source.status)) {
+                    e.code = e.source.status;
+                }
+            }
 
-			fn(e);
-		};
+            fn(e);
+        };
 
-		break;
-	case 'sendstream':
-		xhr.onsendstream = function(e) {
-			if (config.debug) {
-				Ti.API.debug(session, '->', 'Web Service [sendstream <--]');
+        break;
+    case 'sendstream':
+        xhr.onsendstream = function(e) {
+            if (config.debug) {
+                Ti.API.debug(session, '->', 'Web Service [sendstream <--]');
 
-				if (OS_ANDROID) {
-					Ti.API.error(session, '->', 'Sendstream', typeof e, JSON.stringify(e));
-				} else if (OS_IOS) {
-					Ti.API.debug(session, '->', 'Sendstream', typeof e, e);
-				}
-			}
+                if (OS_ANDROID) {
+                    Ti.API.error(session, '->', 'Sendstream', typeof e, JSON.stringify(e));
+                } else if (OS_IOS) {
+                    Ti.API.debug(session, '->', 'Sendstream', typeof e, e);
+                }
+            }
 
-			fn(e);
-		};
+            fn(e);
+        };
 
-		break;
-	case 'datastream':
-		xhr.ondatastream = function(e) {
-			if (config.debug) {
-				Ti.API.debug(session, '->', 'Web Service [datastream <--]');
+        break;
+    case 'datastream':
+        xhr.ondatastream = function(e) {
+            if (config.debug) {
+                Ti.API.debug(session, '->', 'Web Service [datastream <--]');
 
-				if (OS_ANDROID) {
-					Ti.API.error(session, '->', 'Datastream', typeof e, JSON.stringify(e));
-				} else if (OS_IOS) {
-					Ti.API.debug(session, '->', 'Datastream', typeof e, e);
-				}
-			}
+                if (OS_ANDROID) {
+                    Ti.API.error(session, '->', 'Datastream', typeof e, JSON.stringify(e));
+                } else if (OS_IOS) {
+                    Ti.API.debug(session, '->', 'Datastream', typeof e, e);
+                }
+            }
 
-			fn(e);
-		};
+            fn(e);
+        };
 
-		break;
-	};
+        break;
+    };
 }
 
 function request(method, url, input) {
-	if (_.isUndefined(method) || _.isUndefined(url)) {
-		if (config.debug) {
-			Ti.API.error('');
-			Ti.API.error(session, '->', 'Web Service [request -->]');
+    if (_.isUndefined(method) || _.isUndefined(url)) {
+        if (config.debug) {
+            Ti.API.error('');
+            Ti.API.error(session, '->', 'Web Service [request -->]');
 
-			if (_.isUndefined(method)) {
-				Ti.API.error(session, '->', 'Invalid method.');
-			}
+            if (_.isUndefined(method)) {
+                Ti.API.error(session, '->', 'Invalid method.');
+            }
 
-			if (_.isUndefined(url)) {
-				Ti.API.error(session, '->', 'Invalid url.');
-			}
+            if (_.isUndefined(url)) {
+                Ti.API.error(session, '->', 'Invalid url.');
+            }
 
-			Ti.API.error('');
-		}
+            Ti.API.error('');
+        }
 
-		return;
-	}
+        return;
+    }
 
-	if (config.debug) {
-		Ti.API.debug('');
-		Ti.API.debug(session, '->', 'Web Service [request -->]');
-	}
+    if (config.debug) {
+        Ti.API.debug('');
+        Ti.API.debug(session, '->', 'Web Service [request -->]');
+    }
 
-	if (!_.isUndefined(input) && !_.isUndefined(input.body) && _.isObject(input.body)) {
-		for (var i in input.body) {
-			if (_.isNull(input.body[i])) {
-				delete input.body[i];
-			}
-		}
-	}
+    if (!_.isUndefined(input) && !_.isUndefined(input.body) && _.isObject(input.body)) {
+        for (var i in input.body) {
+            if (_.isNull(input.body[i])) {
+                delete input.body[i];
+            }
+        }
+    }
 
-	if (method === 'GET' && !_.isUndefined(input) && !_.isUndefined(input.body) && _.isObject(input.body)) {
-		var symbol = (link.indexOf('?') != -1) ? '&' : '?';
-		var dataQuery = URI.buildget(input.body);
-		dataQuery = (dataQuery.length > 0) ? symbol + dataQuery : '';
-		url = url + dataQuery;
-		delete input.body;
-	}
+    if (method === 'GET' && !_.isUndefined(input) && !_.isUndefined(input.body) && _.isObject(input.body)) {
+        var symbol = (link.indexOf('?') != -1) ? '&' : '?';
+        var dataQuery = URI.buildget(input.body);
+        dataQuery = (dataQuery.length > 0) ? symbol + dataQuery : '';
+        url = url + dataQuery;
+        delete input.body;
+    }
 
-	xhr.open(method, url);
+    xhr.open(method, url);
 
-	var headers = config.headers;
+    var headers = config.headers;
 
-	if (!_.isUndefined(input) && !_.isUndefined(input.header) && _.isObject(input.header)) {
-		headers = _.extend(input.header);
-	}
+    if (!_.isUndefined(input) && !_.isUndefined(input.header) && _.isObject(input.header)) {
+        headers = _.extend(input.header);
+    }
 
-	for (var name in headers) {
-		xhr.setRequestHeader(name, headers[name]);
+    for (var name in headers) {
+        xhr.setRequestHeader(name, headers[name]);
 
-		if (config.debug) {
-			Ti.API.debug(session, '->', 'Header', name, ':', headers[name]);
-		}
-	}
+        if (config.debug) {
+            Ti.API.debug(session, '->', 'Header', name, ':', headers[name]);
+        }
+    }
 
-	if (config.debug) {
-		Ti.API.debug(session, '->', 'Url', ':', url, typeof url + '[' + ((_.isString(url)) ? url.length : 0) + ']');
-		Ti.API.debug(session, '->', 'Method', ':', method, typeof method + '[' + ((_.isString(method)) ? method.length : 0) + ']');
+    if (config.debug) {
+        Ti.API.debug(session, '->', 'Url', ':', url, typeof url + '[' + ((_.isString(url)) ? url.length : 0) + ']');
+        Ti.API.debug(session, '->', 'Method', ':', method, typeof method + '[' + ((_.isString(method)) ? method.length : 0) + ']');
 
-		if (!_.isUndefined(input) && !_.isUndefined(input.body)) {
-			for (var i in input.body) {
-				if (_.isString(input.body[i])) {
-					length = input.body[i].length;
-				} else if (_.isObject(input.body[i])) {
-					length = input.body[i].size;
-				} else {
-					length = 0;
-				}
+        if (!_.isUndefined(input) && !_.isUndefined(input.body)) {
+            for (var i in input.body) {
+                if (_.isString(input.body[i])) {
+                    length = input.body[i].length;
+                } else if (_.isObject(input.body[i])) {
+                    length = input.body[i].size;
+                } else {
+                    length = 0;
+                }
 
-				Ti.API.debug(session, '->', 'Body', i, ':', input.body[i], typeof input.body[i] + '[' + length + ']');
-			}
-		}
+                Ti.API.debug(session, '->', 'Body', i, ':', input.body[i], typeof input.body[i] + '[' + length + ']');
+            }
+        }
 
-		Ti.API.debug('');
-	}
+        Ti.API.debug('');
+    }
 
-	if (!_.isUndefined(input) && !_.isUndefined(input.body) && _.isObject(input.body)) {
-		xhr.send(input.body);
-	} else {
-		xhr.send();
-	}
+    if (!_.isUndefined(input) && !_.isUndefined(input.body) && _.isObject(input.body)) {
+        xhr.send(input.body);
+    } else {
+        xhr.send();
+    }
 
-	return xhr;
+    return xhr;
 };
 
 exports.setTimeout = function(time) {
-	session = creataSession();
-	xhr.setTimeout(time);
+    session = creataSession();
+    xhr.setTimeout(time);
 };
 
 exports.setHeader = function(name, value) {
-	session = creataSession();
-	xhr.setRequestHeader(name, value);
+    session = creataSession();
+    xhr.setRequestHeader(name, value);
 };
 
 exports.abort = function() {
-	session = creataSession();
-	xhr.abort();
+    session = creataSession();
+    xhr.abort();
 };
 
 exports.get = function(url, data) {
-	session = creataSession();
-	return request('GET', url, data);
+    session = creataSession();
+    return request('GET', url, data);
 };
 
 exports.post = function(url, data) {
-	session = creataSession();
-	return request('POST', url, data);
+    session = creataSession();
+    return request('POST', url, data);
 };
 
 exports.put = function(url, data) {
-	session = creataSession();
-	return request('PUT', url, data);
+    session = creataSession();
+    return request('PUT', url, data);
 };
 
 exports.delete = function(url, data) {
-	session = creataSession();
-	return request('DELETE', url, data);
+    session = creataSession();
+    return request('DELETE', url, data);
 };
 
 exports.success = function(fn) {
-	session = creataSession();
-	callback('success', fn);
+    session = creataSession();
+    callback('success', fn);
 };
 
 exports.error = function(fn) {
-	session = creataSession();
-	callback('error', fn);
+    session = creataSession();
+    callback('error', fn);
 };
 
 exports.sendstream = function(fn) {
-	session = creataSession();
-	callback('sendstream', fn);
+    session = creataSession();
+    callback('sendstream', fn);
 };
 
 exports.datastream = function(fn) {
-	session = creataSession();
-	callback('datastream', fn);
+    session = creataSession();
+    callback('datastream', fn);
 };
