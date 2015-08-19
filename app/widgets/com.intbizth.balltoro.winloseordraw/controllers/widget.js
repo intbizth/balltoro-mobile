077b1acdc7b4ae08a62b87c04af25c48fd37d34d
iitum <watcharaphong@intbizth.com>
2015-08-19 14:58:50 +0700

    [com.intbizth.balltoro.winloseordraw] change format

--- app/widgets/com.intbizth.balltoro.winloseordraw/controllers/widget.js
+++ app/widgets/com.intbizth.balltoro.winloseordraw/controllers/widget.js
@@ -2,122 +2,122 @@ var config = require(WPATH('config'));
 var timer = null;
 
 function createWin() {
-	var view = Ti.UI.createView({
-		width : config.width,
-		height : config.height,
-		backgroundColor : config.win.backgroundColor
-	});
+    var view = Ti.UI.createView({
+        width : config.width,
+        height : config.height,
+        backgroundColor : config.win.backgroundColor
+    });
 
-	var label = Ti.UI.createLabel({
-		width : config.width,
-		height : config.height,
-		font : {
-			fontSize : 10
-		},
-		color : config.win.color,
-		text : L('com.intbizth.balltoro.winloseordraw.win'),
-		textAlign : 'center'
-	});
+    var label = Ti.UI.createLabel({
+        width : config.width,
+        height : config.height,
+        font : {
+            fontSize : 10
+        },
+        color : config.win.color,
+        text : L('com.intbizth.balltoro.winloseordraw.win'),
+        textAlign : 'center'
+    });
 
-	view.add(label);
+    view.add(label);
 
-	return view;
+    return view;
 };
 
 function createDraw() {
-	var view = Ti.UI.createView({
-		width : config.width,
-		height : config.height,
-		backgroundColor : config.draw.backgroundColor
-	});
+    var view = Ti.UI.createView({
+        width : config.width,
+        height : config.height,
+        backgroundColor : config.draw.backgroundColor
+    });
 
-	var label = Ti.UI.createLabel({
-		width : config.width,
-		height : config.height,
-		font : {
-			fontSize : 10
-		},
-		color : config.draw.color,
-		text : L('com.intbizth.balltoro.winloseordraw.draw'),
-		textAlign : 'center'
-	});
+    var label = Ti.UI.createLabel({
+        width : config.width,
+        height : config.height,
+        font : {
+            fontSize : 10
+        },
+        color : config.draw.color,
+        text : L('com.intbizth.balltoro.winloseordraw.draw'),
+        textAlign : 'center'
+    });
 
-	view.add(label);
+    view.add(label);
 
-	return view;
+    return view;
 };
 
 function createLose() {
-	var view = Ti.UI.createView({
-		width : config.width,
-		height : config.height,
-		backgroundColor : config.lose.backgroundColor
-	});
+    var view = Ti.UI.createView({
+        width : config.width,
+        height : config.height,
+        backgroundColor : config.lose.backgroundColor
+    });
 
-	var label = Ti.UI.createLabel({
-		width : config.width,
-		height : config.height,
-		font : {
-			fontSize : 10
-		},
-		color : config.lose.color,
-		text : L('com.intbizth.balltoro.winloseordraw.lose'),
-		textAlign : 'center'
-	});
+    var label = Ti.UI.createLabel({
+        width : config.width,
+        height : config.height,
+        font : {
+            fontSize : 10
+        },
+        color : config.lose.color,
+        text : L('com.intbizth.balltoro.winloseordraw.lose'),
+        textAlign : 'center'
+    });
 
-	view.add(label);
+    view.add(label);
 
-	return view;
+    return view;
 };
 
 exports.startTest = function(duration, element) {
-	run();
+    run();
 
-	timer = setInterval(function() {
-		run();
-	}, duration);
+    timer = setInterval(function() {
+        run();
+    }, duration);
 
-	function run() {
-		element.removeAllChildren();
+    function run() {
+        element.removeAllChildren();
 
-		var random = _.random(1, 10);
+        var random = _.random(1, 10);
 
-		for (var i = 1; i <= random; i++) {
-			var random2 = _.random(0, 2);
+        for (var i = 1; i <= random; i++) {
+            var random2 = _.random(0, 2);
 
-			if (random2 === 0) {
-				element.add(createWin());
-			} else if (random2 === 1) {
-				element.add(createLose());
-			} else if (random2 === 2) {
-				element.add(createDraw());
-			}
+            if (random2 === 0) {
+                element.add(createWin());
+            } else if (random2 === 1) {
+                element.add(createLose());
+            } else if (random2 === 2) {
+                element.add(createDraw());
+            }
 
-			if (i < random) {
-				element.add(Ti.UI.createView({
-					width : 2,
-					height : 1
-				}));
-			}
-		}
-	};
+            if (i < random) {
+                element.add(Ti.UI.createView({
+                    width : 2,
+                    height : 1
+                }));
+            }
+        }
+    };
 };
 
 exports.stopTest = function(element) {
-	clearInterval(timer);
-	timer = null;
+    clearInterval(timer);
+    timer = null;
 
-	element.removeAllChildren();
+    element.removeAllChildren();
 };
 
 exports.createWin = function() {
-	return createWin();
+    return createWin();
 };
 
 exports.createLose = function() {
-	return createLose();
+    return createLose();
 };
 
 exports.createDraw = function() {
-	return createDraw();
+    return createDraw();
 };
